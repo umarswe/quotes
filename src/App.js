1a653eae6c6a4f4811f329578849eb508e975223
umaar-kr <umaar.kr@gmail.com>
2023-11-04 22:43:00 +0900

    adding previous button

--- src/App.js
+++ src/App.js
@@ -2,26 +2,42 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [quote, setQuote] = useState("");
+  const [quotes, setQuotes] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  async function getQuotes() {
-    const res = await fetch("https://api.adviceslip.com/advice");
-    const data = await res.json();
-    setQuote(data.slip.advice);
-  }
+  useEffect(() => {
+    async function getQuotes() {
+      const res = await fetch("https://api.adviceslip.com/advice");
+      const data = await res.json();
+      setQuotes((q) => [...q, data.slip.advice]);
+    }
 
-  useEffect(function () {
     getQuotes();
-  }, []);
+  }, [activeIndex]);
 
-  getQuotes();
+  // TODO: Learn why we used [activeIndex] in useEffect
+  // TODO: Learn what this line does: setQuotes((q) => [...q, data.slip.advice]);
+  // TODO: Relearn and practise spread operator
 
   return (
     <div className="App">
-      <h1 className="quotes">{quote}</h1>
-      <button onClick={getQuotes} className="quotes__btn">
-        Next
-      </button>
+      <h1 className="quotes">{quotes[activeIndex]}</h1>
+      {/* TODO: Hide Previous button when index becomes less than 0  */}
+      <div className="quotes__button-group">
+        <button
+          onClick={() => setActiveIndex(activeIndex - 1)}
+          className="quotes__btn quotes__btn--previous"
+        >
+          Previous
+        </button>
+
+        <button
+          onClick={() => setActiveIndex(activeIndex + 1)}
+          className="quotes__btn quotes__btn--next"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
